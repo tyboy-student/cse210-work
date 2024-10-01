60db90204baeba97a06abc6a7e883cd1dcd96f84
tyboy <33986235+tyboy-student@users.noreply.github.com>
2024-10-01 11:06:53 -0600

    Finished the Prep 3 work for week 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        bool continueLoop = true;
+        Random rng = new Random();
+        int magicNumber;
+        magicNumber = rng.Next(100);
+        int userGuess;
+
+        while (true)
+        {
+            Console.Write("Guess the Magic Number: ");
+            userGuess = int.Parse(Console.ReadLine());
+            if (userGuess == magicNumber)
+            {
+                // leave the loop if the number is correct
+                break;
+            }            
+            // Assume we didn't break the loop
+            Console.WriteLine("Not Quite");
+            if (userGuess < magicNumber) Console.WriteLine("You need to go higher");
+            else Console.WriteLine("You need to go lower");
+        }
+        Console.WriteLine("Correct!");
+        // Wait on the user to hit enter to close the program
+        Console.Write("Press ENTER to end program");
+        Console.ReadLine();
     }
 }
