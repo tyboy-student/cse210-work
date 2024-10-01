8cd3dcb72a7e599a97a76e6c4a19587d8d236c9c
tyboy <33986235+tyboy-student@users.noreply.github.com>
2024-10-01 11:20:10 -0600

    Finished the base assignment for prep 4 (Not the challenge stuffs, I'll loop back around later)

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("You will be asked to input multiple integer values. It will stop collecting once you enter '0'.\n");
+        while (true)
+        {
+            Console.Write("Enter an integer value: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            numbers.Add(userNumber);
+            if (userNumber == 0) break;
+        }
+
+        // Give the sum, average, and max numbers
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The Average is: {numbers.Average()}");
+        Console.WriteLine($"The Largest number is: {numbers.Max()}");
+        Console.WriteLine($"The smallest positive number is:");
     }
 }
